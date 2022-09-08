69b5bb0b6400a1afe5b87aec221573b1eda1292b
ndelanhese <nnathanh3@gmail.com>
2022-09-08 15:11:26 -0300

    chore(login): redirect if nonexistent token

--- src/features/Dashboard/Dashboard.tsx
+++ src/features/Dashboard/Dashboard.tsx
@@ -1,12 +1,7 @@
 import { useContext, useEffect } from 'react';
 
-import { GetServerSideProps } from 'next';
-
-import { parseCookies } from 'nookies';
-
 import { AuthContext } from '../../contexts/AuthContexts';
 import { NavBar } from '../NavBar/NavBar';
-import { getAPIClient } from '../../services/axios';
 import { api } from '../../services/api';
 
 export const Dashboard = () => {
@@ -17,22 +12,4 @@ export const Dashboard = () => {
   });
 
   return <NavBar avatar_url={userState?.avatar_url} logOut={logOut} />;
-};
-
-export const getServerSideProps: GetServerSideProps = async ctx => {
-  const apiClient = getAPIClient(ctx);
-  const { 'NextAuth.token': token } = parseCookies(ctx);
-
-  if (!token) {
-    return {
-      redirect: {
-        destination: '/',
-        permanent: false,
-      },
-    };
-  }
-  await apiClient.get('/user');
-  return {
-    props: {},
-  };
 };
