fc8f7d9e42543d6946b5ec61af79296e980865a3
ndelanhese <nnathanh3@gmail.com>
2022-09-08 08:38:53 -0300

    chore(index login): implement the colormode on the index login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,10 +12,16 @@ import {
   Button,
   Heading,
   Text,
+  useColorModeValue,
 } from '@chakra-ui/react';
 
 const Home: NextPage = () => (
-  <Flex minH="100vh" align="center" justify="center" bg="gray.800">
+  <Flex
+    minH="100vh"
+    align="center"
+    justify="center"
+    bg={useColorModeValue('gray.50', 'gray.800')}
+  >
     <Stack spacing={8} mx="auto" maxW="lg" py={12} px={6}>
       <Stack align="center">
         <Heading fontSize="4xl">Sign in to your account</Heading>
@@ -23,7 +29,12 @@ const Home: NextPage = () => (
           to enjoy all of our cool <Link color="blue.400">features</Link> ✌️
         </Text>
       </Stack>
-      <Box rounded="lg" bg="gray.700" boxShadow="lg" p={8}>
+      <Box
+        rounded="lg"
+        boxShadow="lg"
+        p={8}
+        bg={useColorModeValue('white', 'gray.700')}
+      >
         <Stack spacing={4}>
           <FormControl id="email">
             <FormLabel>Email address</FormLabel>
